a41b6148f53dba8dd65477d35011f512a86657d3
Keyur Raval <keyur.raval18@gmail.com>
2023-10-10 12:27:45 +0530

    add: test cases, remove console

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 const numberMaps = [
-  { id: 0, text: "zero"},
-  { id: 1, text: "one" },
-  { id: 2, text: "two" },
-  { id: 3, text: "three" },
-  { id: 4, text: "four" },
-  { id: 5, text: "five" },
-  { id: 6, text: "six" },
-  { id: 7, text: "seven" },
-  { id: 8, text: "eight" },
-  { id: 9, text: "nine" },
-  { id: 10, text: "ten" },
-  { id: 11, text: "eleven" },
-  { id: 12, text: "twelve" },
-  { id: 13, text: "thirteen" },
-  { id: 14, text: "fourteen" },
-  { id: 15, text: "fifteen" },
-  { id: 16, text: "sixteen" },
-  { id: 17, text: "seventeen" },
-  { id: 18, text: "eighteen" },
-  { id: 19, text: "nineteen" },
-  { id: 20, text: "twenty" },
-  { id: 21, text: "twenty-one" },
-  { id: 22, text: "twenty-two" },
-  { id: 23, text: "twenty-three" },
-  { id: 24, text: "twenty-four" },
-  { id: 25, text: "twenty-five" },
-  { id: 26, text: "twenty-six" },
-  { id: 27, text: "twenty-seven" },
-  { id: 28, text: "twenty-eight" },
-  { id: 29, text: "twenty-nine" },
-  { id: 30, text: "thirty" },
-  { id: 31, text: "thirty-one" },
-  { id: 32, text: "thirty-two" },
-  { id: 33, text: "thirty-three" },
-  { id: 34, text: "thirty-four" },
-  { id: 35, text: "thirty-five" },
-  { id: 36, text: "thirty-six" },
-  { id: 37, text: "thirty-seven" },
-  { id: 38, text: "thirty-eight" },
-  { id: 39, text: "thirty-nine" },
-  { id: 40, text: "forty" },
-  { id: 41, text: "forty-one" },
-  { id: 42, text: "forty-two" },
-  { id: 43, text: "forty-three" },
-  { id: 44, text: "forty-four" },
-  { id: 45, text: "forty-five" },
-  { id: 46, text: "forty-six" },
-  { id: 47, text: "forty-seven" },
-  { id: 48, text: "forty-eight" },
-  { id: 49, text: "forty-nine" },
-  { id: 50, text: "fifty" },
-  { id: 51, text: "fifty-one" },
-  { id: 52, text: "fifty-two" },
-  { id: 53, text: "fifty-three" },
-  { id: 54, text: "fifty-four" },
-  { id: 55, text: "fifty-five" },
-  { id: 56, text: "fifty-six" },
-  { id: 57, text: "fifty-seven" },
-  { id: 58, text: "fifty-eight" },
-  { id: 59, text: "fifty-nine" },]
+  { id: 0, text: 'zero' },
+  { id: 1, text: 'one' },
+  { id: 2, text: 'two' },
+  { id: 3, text: 'three' },
+  { id: 4, text: 'four' },
+  { id: 5, text: 'five' },
+  { id: 6, text: 'six' },
+  { id: 7, text: 'seven' },
+  { id: 8, text: 'eight' },
+  { id: 9, text: 'nine' },
+  { id: 10, text: 'ten' },
+  { id: 11, text: 'eleven' },
+  { id: 12, text: 'twelve' },
+  { id: 13, text: 'thirteen' },
+  { id: 14, text: 'fourteen' },
+  { id: 15, text: 'fifteen' },
+  { id: 16, text: 'sixteen' },
+  { id: 17, text: 'seventeen' },
+  { id: 18, text: 'eighteen' },
+  { id: 19, text: 'nineteen' },
+  { id: 20, text: 'twenty' },
+  { id: 21, text: 'twenty-one' },
+  { id: 22, text: 'twenty-two' },
+  { id: 23, text: 'twenty-three' },
+  { id: 24, text: 'twenty-four' },
+  { id: 25, text: 'twenty-five' },
+  { id: 26, text: 'twenty-six' },
+  { id: 27, text: 'twenty-seven' },
+  { id: 28, text: 'twenty-eight' },
+  { id: 29, text: 'twenty-nine' },
+  { id: 30, text: 'thirty' },
+  { id: 31, text: 'thirty-one' },
+  { id: 32, text: 'thirty-two' },
+  { id: 33, text: 'thirty-three' },
+  { id: 34, text: 'thirty-four' },
+  { id: 35, text: 'thirty-five' },
+  { id: 36, text: 'thirty-six' },
+  { id: 37, text: 'thirty-seven' },
+  { id: 38, text: 'thirty-eight' },
+  { id: 39, text: 'thirty-nine' },
+  { id: 40, text: 'forty' },
+  { id: 41, text: 'forty-one' },
+  { id: 42, text: 'forty-two' },
+  { id: 43, text: 'forty-three' },
+  { id: 44, text: 'forty-four' },
+  { id: 45, text: 'forty-five' },
+  { id: 46, text: 'forty-six' },
+  { id: 47, text: 'forty-seven' },
+  { id: 48, text: 'forty-eight' },
+  { id: 49, text: 'forty-nine' },
+  { id: 50, text: 'fifty' },
+  { id: 51, text: 'fifty-one' },
+  { id: 52, text: 'fifty-two' },
+  { id: 53, text: 'fifty-three' },
+  { id: 54, text: 'fifty-four' },
+  { id: 55, text: 'fifty-five' },
+  { id: 56, text: 'fifty-six' },
+  { id: 57, text: 'fifty-seven' },
+  { id: 58, text: 'fifty-eight' },
+  { id: 59, text: 'fifty-nine' },
+];
 
 const numberToText = (id) => {
-  const value = numberMaps.find(numberMap => numberMap.id == id);
-  console.log({value});
+  const value = numberMaps.find((numberMap) => numberMap.id == id);
   return value.text;
-}
+};
 function convertTimeToWords(time) {
   // TODO: real code goes here!
 
@@ -73,33 +73,36 @@ function convertTimeToWords(time) {
 
   const hours = parseInt(timeArray[0], 10);
   const minutes = parseInt(timeArray[1], 10);
-  
-  const hour = numberToText(hours);
-  const minute = numberToText(minutes);
+
+  const hourInText = numberToText(hours);
+  const minuteInText = numberToText(minutes);
 
   if (minutes === 0) {
     if (hours === 0) {
       return 'midnight';
     } else if (hours === 12) {
       return 'midday';
-    } 
-    
-    return `${hour} o'clock`;
-  }else if (minute == 15) {
-    return `quarter past ${hour}`
+    } else {
+      return `${hourInText} o'clock`;
+    }
+  } if (minutes === 15) {
+    return `quarter past ${hourInText}`;
   } else if (minutes < 29) {
-    return `${minute} past ${hour}`
-  }  else if (minute == 30) {
-    return `half past ${hour}`
-  }  else if (minutes == 45) {
-    console.log(hours);
-    return `quarter to ${numberToText(hours + 1)}`
-  } else if (minutes > 30) {
+    return `${minuteInText} past ${hourInText}`;
+  } else if (minutes === 30) {
+    return `half past ${hourInText}`;
+  } else if (minutes === 45) {
+    return `quarter to ${numberToText(hours + 1)}`;
+  } else {
     const previousMinutes = 60 - minutes;
-    return `${numberToText(previousMinutes)} to ${numberToText(hours + 1)}`
+    const nextHour = hours + 1;
+    if (nextHour === 12) {
+      return `${numberToText(previousMinutes)} to midday`;
+    } else if (nextHour === 24) {
+      return `${numberToText(previousMinutes)} to midnight`;
+    }
+    return `${numberToText(previousMinutes)} to ${numberToText(hours + 1)}`;
   }
-
-  return 'half past eight';
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -16,8 +16,69 @@ describe('Time to words', () => {
     expect(timeInWords).toBe('quarter to three');
   });
 
-  it.only('Handles time after 30 mins - 2:33', () => {
+  it('Handles time after 30 mins - 2:33', () => {
     const timeInWords = convertTimeToWords('2:33');
     expect(timeInWords).toBe('twenty-seven to three')
   })
+
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('Handles o\'clock', () => {
+    const timeInWords = convertTimeToWords('2:00');
+    expect(timeInWords).toBe('two o\'clock');
+  });
+
+  it('Handles past', () => {
+    const timeInWords = convertTimeToWords('2:03');
+    expect(timeInWords).toBe('three past two');
+  });
+
+  it('Handles past', () => {
+    const timeInWords = convertTimeToWords('2:11');
+    expect(timeInWords).toBe('eleven past two');
+  });
+
+  it('Handles quarter past', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
+  it('Handles half past', () => {
+    const timeInWords = convertTimeToWords('2:30');
+    expect(timeInWords).toBe('half past two');
+  });
+
+  it('Handles to', () => {
+    const timeInWords = convertTimeToWords('2:40');
+    expect(timeInWords).toBe('twenty to three');
+  });
+
+  it('Handles quarter to', () => {
+    const timeInWords = convertTimeToWords('2:45');
+    expect(timeInWords).toBe('quarter to three');
+  });
+
+  it('Handles five to', () => {
+    const timeInWords = convertTimeToWords('2:55');
+    expect(timeInWords).toBe('five to three');
+  });
+
+  it('Handles twenty-seven to', () => {
+    const timeInWords = convertTimeToWords('2:33');
+    expect(timeInWords).toBe('twenty-seven to three');
+  });
+
+  it('Handles twenty-three to midday', () => {
+    const timeInWords = convertTimeToWords('11:37');
+    expect(timeInWords).toBe('twenty-three to midday');
+  });
+
+  it('Handles twenty-three to midnight', () => {
+    const timeInWords = convertTimeToWords('23:37');
+    expect(timeInWords).toBe('twenty-three to midnight');
+  });
+  
 });
